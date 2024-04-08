552ecfa5131d0c23a4bc9b4cbee6f5366006ccd9
zhudi <zhudi@rd.netease.com>
2024-04-08 16:24:57 +0800

    mod: add constuctor

--- main.cpp
+++ main.cpp
@@ -24,34 +24,63 @@ struct Obj
 {
     int a;
     std::string b;
+
+    // 默认构造函数
     Obj() = default;
+
+    // 带参构造函数
     Obj(int a, const std::string &b) : a(a), b(b)
     {
         LOGD << "Construct:" << this << std::endl;
     }
+
+    // 拷贝构造函数
+    Obj(const Obj &other) : a(other.a), b(other.b)
+    {
+        MARK("Copy construct")
+    }
+
+    // 移动构造函数
+    Obj(Obj &&other) noexcept : a(std::move(other.a)), b(std::move(other.b))
+    {
+        MARK("Move construct")
+        other.a = 0; // 将源对象的成员变量设置为有效状态
+        other.b.clear();
+    }
+
+    // 析构函数
     ~Obj()
     {
         LOGD << "Destruct:" << this << std::endl;
     }
-    Obj(Obj &&other)
+
+    // 赋值运算符
+    Obj& operator=(const Obj &other)
     {
-        MARK("Move construct")
-        a = std::move(other.a);
-        b = std::move(other.b);
-    }
-    Obj(const Obj &other)
-    {
-        if (&other != this)
+        if (this != &other)
         {
-            MARK("Copy construct")
+            MARK("Copy assign")
             a = other.a;
             b = other.b;
         }
+        return *this;
     }
-    void me() const
+
+    // 移动赋值运算符
+    Obj& operator=(Obj &&other) noexcept
     {
-        ARGS(this);
+        if (this != &other)
+        {
+            MARK("Move assign")
+            a = std::move(other.a);
+            b = std::move(other.b);
+            other.a = 0; // 将源对象的成员变量设置为有效状态
+            other.b.clear();
+        }
+        return *this;
     }
+
+    // 类型转换运算符
     explicit operator bool() const
     {
         MARK("bool")
